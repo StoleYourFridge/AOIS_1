e767916cc84c1894468ef59df34cf0a713fd80dd
StoleYourFridge <stoleyourfridge@gmail.com>
2021-09-17 10:07:04 +0300

    straight is done

--- АОИС_1.cpp
+++ АОИС_1.cpp
@@ -1,14 +1,50 @@
 ﻿#include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
+const int Size = 8;
+
 class MyNumber
 {
-    vector<int>doub;
+public:
+    bool positive = true;
+    vector<int>straight;
+    vector<int>reverse;
+    vector<int>additional;
+
+public:
+    MyNumber (int number)
+    {
+        if (number < 0) positive = false;
+        number = abs(number);
+        while (true)
+        {
+            if (number != 1) straight.insert(straight.begin(), number % 2);
+            else {
+                straight.insert(straight.begin(), 1);
+                break;
+            }
+            number /= 2;
+        }
+        int zeroamount = Size - straight.size() - 1;
+        for (int i = 0; i < zeroamount; i++)
+        {
+            straight.insert(straight.begin(), 0);
+        }
+        if (positive) straight.insert(straight.begin(), 0);
+        else straight.insert(straight.begin(), 1);
+    }
+
 };
 
 int main()
 {
-    
+    MyNumber f(12);
+    for (int i = 0; i < f.straight.size(); i++)
+    {
+        cout << f.straight[i];
+    }
+    return 0;
 }
