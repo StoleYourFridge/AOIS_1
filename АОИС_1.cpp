56a0e51e692d0d2ceecf479f8356fb0da789d8da
StoleYourFridge <stoleyourfridge@gmail.com>
2021-09-24 02:00:38 +0300

    constructor and print function created

--- АОИС_1.cpp
+++ АОИС_1.cpp
@@ -1,50 +1,97 @@
 ﻿#include <iostream>
 #include <vector>
 #include <cmath>
+#include <stack>
 
 using namespace std;
 
-const int Size = 8;
+const int Size = 32;
 
 class MyNumber
 {
-public:
+private:
     bool positive = true;
     vector<int>straight;
     vector<int>reverse;
     vector<int>additional;
 
 public:
-    MyNumber (int number)
-    {
-        if (number < 0) positive = false;
-        number = abs(number);
-        while (true)
-        {
-            if (number != 1) straight.insert(straight.begin(), number % 2);
-            else {
-                straight.insert(straight.begin(), 1);
-                break;
-            }
-            number /= 2;
-        }
-        int zeroamount = Size - straight.size() - 1;
-        for (int i = 0; i < zeroamount; i++)
-        {
-            straight.insert(straight.begin(), 0);
-        }
-        if (positive) straight.insert(straight.begin(), 0);
-        else straight.insert(straight.begin(), 1);
-    }
-
+    MyNumber(int number);
+    void print();
 };
+
+MyNumber::MyNumber(int number)
+{
+    if (number < 0) positive = false;
+    number = abs(number);
+    while (true)
+    {
+        if (number != 1) straight.insert(straight.begin(), number % 2);
+        else {
+            straight.insert(straight.begin(), 1);
+            break;
+        }
+        number /= 2;
+    }
+    int zeroamount = Size - straight.size() - 1;
+    for (int i = 0; i < zeroamount; i++)
+    {
+        straight.insert(straight.begin(), 0);
+    }
+    if (positive) {
+        straight.insert(straight.begin(), 0);
+        reverse = straight;
+        additional = straight;
+    }
+    else {
+        straight.insert(straight.begin(), 1);
+        reverse = straight;
+        additional = straight;
+        for (int i = 1; i < straight.size(); i++)
+        {
+            if (straight[i] == 1) additional[i] = reverse[i] = 0;
+            else additional[i] = reverse[i] = 1;
+        }
+        bool mind = true;
+        int i = 31;
+        while (mind)
+        {
+            if (additional[i] == 0)
+            {
+                additional[i] = 1;
+                mind = false;
+            }
+
+            else if (additional[i] == 1) {
+                additional[i] = 0;
+                i--;
+            }
+            if (i == -1) i = 31;
+        }
+    }
+}
+void MyNumber::print()
+{
+    cout << "Straight :   ";
+    for (int i = 0; i < straight.size(); i++)
+    {
+        cout << straight[i];
+    }
+    cout << endl << "Reverse :    ";
+    for (int i = 0; i < reverse.size(); i++)
+    {
+        cout << reverse[i];
+    }
+    cout << endl << "Additional : ";
+    for (int i = 0; i < additional.size(); i++)
+    {
+        cout << additional[i];
+    }
+}
 
 int main()
 {
-    MyNumber f(12);
-    for (int i = 0; i < f.straight.size(); i++)
-    {
-        cout << f.straight[i];
-    }
+    MyNumber f(-12);
+    f.print();
     return 0;
 }
